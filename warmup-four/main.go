a78fbd0f7067947129ea62bc46c47bd82120c2b5
Amir Behmaram <amirbehmaram4@gmail.com>
2021-01-11 20:57:09 -0800

    Wwarmup Four Solution

---
+++ warmup-four/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+// Complete the jumpingOnClouds function below.
+func jumpingOnClouds(c []int32) int32 {
+    var numJumps int32 = 0
+    index := 0
+
+    for index < len(c) {
+        
+        // Check if we can jump two or one 
+        if (index + 2 < len(c) && c[index + 2] == 0) {
+            numJumps++
+            index = index + 2
+            
+        } else if (index + 1 < len(c) && c[index + 1] == 0) {
+            numJumps++
+            index = index + 1
+            
+        } else if (index < len(c)) { // Our terminating condition
+            index++
+        }
+    }
+    
+    return numJumps
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+
+    cTemp := strings.Split(readLine(reader), " ")
+
+    var c []int32
+
+    for i := 0; i < int(n); i++ {
+        cItemTemp, err := strconv.ParseInt(cTemp[i], 10, 64)
+        checkError(err)
+        cItem := int32(cItemTemp)
+        c = append(c, cItem)
+    }
+
+    result := jumpingOnClouds(c)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
